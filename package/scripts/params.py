551c2f811172233d606b55b9144da61653c2424d
Denys Havrysh <Denys.Gavrysh@gmail.com>
2016-04-14 17:05:40 +0300

    Create a user with password in Mongo database if specified

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -1,9 +1,9 @@
 from resource_management import *
 
-config = Script.get_config()
-
-db_path = default('configurations/mongodb/db_path', '/var/lib/mongo')
 bind_ip = default('configurations/mongodb/bind_ip', '0.0.0.0')
-# The web status page is always accessible at a port number that is 1000 greater than the port determined by tcp_port.
 tcp_port = default('configurations/mongodb/tcp_port', '27017')
+db_path = default('configurations/mongodb/db_path', '/var/lib/mongo')
+db_name = default('configurations/mongodb/db_name', '')
+db_user = default('configurations/mongodb/db_user', 'anadmin')
+db_pass = default('configurations/mongodb/db_pass', '')
 mongo_host = default('/clusterHostInfo/mongodb_master_hosts', ['unknown'])[0]
